2a9540320c2c08f35aa03fa67da3182394bc39c1
giogoa <qristinekintsurashvili57@gmail.com>
2024-06-27 19:24:38 +0400

    commit

---
+++ GOA/level 13/homework/k.py
@@ -0,0 +1,32 @@
+#დავალება N1
+x = int(input("what is the first number?"))
+q = int(input("what is the second number"))
+w = int(input("what is the secong number"))
+c = int(input("what is the second number"))
+print(x * w )
+print(x / c)
+print(q + x)
+print(x - w)
+
+#დავალება N2
+my_age = int(input("how old are you?> "))
+my_moms_age = int(input("how old is she?>"))
+my_brother_age =int(input("how old is he?>"))
+my_sisters_age = int(input("how old is she?>"))
+my_dad_age = int(input("how old is he?> "))
+
+#დავალება N3
+name = "giorgi"
+surname = "kintsurashvili"
+age = 12
+Country = "georgia"
+hobby = "volleyball"
+
+print(f"My name is {name}. My surname is {surname}. I am {age} years old. I am from {Country}. My hobby is {hobby}")
+
+
+
+
+#დავალება N4
+o = (input("what is that name?> "))
+print(f"happy birthday  to {o}. i wish you everything best.")
