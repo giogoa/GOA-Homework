cf1ec67ca23fc863edf1219850520504083ad9d6
giogoa <qristinekintsurashvili57@gmail.com>
2024-06-27 19:20:38 +0400

    Delete Level 01 directory

--- Level 01/homework/its.py
+++
@@ -1,7 +0,0 @@
-Name ="giorgi"
-lastname="kintsurashvili"
-age=" 12 years old"
-favouritesubject=" loves sport"
-studys=" studys programming"
-
-print(Name + lastname + age + favouritesubject + studys)
