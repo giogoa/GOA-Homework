abf531b9b34e2530430ebe64741bb3b1289d96d2
giogoa <qristinekintsurashvili57@gmail.com>
2024-07-10 20:59:37 +0400

    commit

---
+++ GOA/level17/homework/input.py
@@ -0,0 +1,51 @@
+#N1
+
+name1 = int(input("enter number from 1 to 1000"))
+name2 = int(input("enter number from 1 to 1000")) 
+print(name1 > name2)
+
+#N1s n2
+
+item = int(input("enter number"))
+item price = int(input("enter items price"))
+
+print(item > 100 and item < item price)
+
+#N1s n3s
+
+age = int(input("pls enter ur age"))
+purchase_amount = float(input("enter ur purchase amount"))
+
+print(age > 60 and purchase_amount > 100)
+#you can use discount
+print(age < 60 and purchase_amount < 100)
+#you cant use discount
+
+#N1 n4s
+
+gpa = int(input("enter ur gpa"))
+income = int(input("enter ur familys money amount"))
+
+print(gpa > 3.5)
+# თქვენ შეგიძლიათ მიიღოთ უმაღლესი სტიპენდია
+print(income < 50000)
+# სტიპენდიის ოდენობა:10000
+#N1 SUCCESFULLY IS DONE
+
+#N2
+numb1 = int(input("enter number"))
+numb2 = int(input("enter number"))
+
+print(numb1 + numb2)
+print(numb1 - numb2)
+print(numb1 * numb2)
+print(numb1 // numb2)
+#N2 SUCCESFULLY IS DONE
+
+#N3
+mark1 = float(input("enter ur science mark"))
+mark2 = float(input("enter ur english mark"))
+
+print((mark1 + mark2) // 2)
+
+
