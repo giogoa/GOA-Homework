ef04a9089646fb3dbe057327a660c6603e23ce37
giogoa <qristinekintsurashvili57@gmail.com>
2024-08-08 21:34:34 +0400

    Add files via upload

---
+++ GOA/level25/homework/aiaiaia.py
@@ -0,0 +1,32 @@
+#1)
+for i in range(23):
+    print(i)
+    
+#2)
+for i in range(12):
+    print("GOA is best")
+    
+#3)
+for i in range(23):
+    print()
+    
+#4)
+for i in range(1 , 35 , 5):
+    print(f"this is numbers {i}")
+    
+#5)
+number = int(input("enter time: "))
+while number >= 0:
+    number = number - 1
+    print(number + 1)
+
+#6)
+for i in range(7):
+    for x in range(6):
+        print(f"i={i}, j={x}")
+        
+#7)
+for i in range(1, 6):
+    print(f"{i} squared is {i ** 2}")
+
+#*
